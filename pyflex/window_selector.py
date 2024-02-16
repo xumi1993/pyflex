065975ae9ff6b19a77b0fb2c0623fc9a962a8b0e
bch0w <bryant.h.c@gmail.com>
2024-02-16 11:49:45 -0900

    added 's' after time shift log message

--- pyflex/window_selector.py
+++ pyflex/window_selector.py
@@ -794,7 +794,7 @@ class WindowSelector(object):
             if not (tshift_min < win.cc_shift *
                     self.observed.stats.delta < tshift_max):
                 _cc_shift_sec = win.cc_shift * self.synthetic.stats.delta
-                logger.debug("Window rejected due to time shift: %.2f" %
+                logger.debug("Window rejected due to time shift: %.2fs" %
                              _cc_shift_sec)
                 return False
             return True
